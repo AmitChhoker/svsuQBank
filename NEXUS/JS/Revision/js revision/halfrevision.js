fc9fe664b276b223f74f3cf891689896f7ae3055
Amit Chhoker <amitchhoker758@gmail.com>
2025-03-25 21:16:44 +0530

    amit1 commit

--- NEXUS/JS/Revision/js revision/halfrevision.js
+++ NEXUS/JS/Revision/js revision/halfrevision.js
@@ -1,89 +1,23 @@
-// console.log("Hello world");
-// let obj={
-//     name:"Amit",
-//     age:13,
-//     class:"12th"
-// }
-// obj.name="Kapil"
-// console.log(obj);
-
-//number
-// let num=null;
-// console.log(typeof num);
-
-// console.log(null==undefined);
-// console.log(null==0);
-// console.log(undefined==0);
-
-// console.log(null==0);
-// console.log(null>0);
-// console.log(null>0);
-// console.log(null<=0);
-// console.log(null>=0);
-
-// console.log(NaN==NaN);
-
-
-//Convert from number to string 
-// let n=100;
-// let num=n.toString();
-// console.log(typeof n)
-// console.log(typeof num)
-
-//Array ka declaration 
-// let arr=[10,20,304,5];
-// console.log(arr);
-// console.log(typeof arr);
-
-// let arr=[10,20,30,40,50,60];
-
-// let a="10";
-// console.log(Number(a));
-
-// let str="Amit Chhoker";
-// console.log(str);
-
-// let str="Amit Chhoker";
-// console.log(str.length);
-
-//searchin of character
-// let str="Amit Chhoker ki hal chal tere tu suna m to bdhiya";
-// console.log(str.indexOf("chal"));
-
-// let str="Amit Chhoker ki hal chal tere tu suna m to bdhiya";
-// console.log(str.slice(1,10));
-
-// let num=23.122;
-// console.log(num.toFixed(2));
-// console.log(num.toPrecision(2));
-
-// console.log(Math.floor(Math.random()*(21)+30));
-
-//Array
-// const arr=new Array();
-// arr.push(30);
-// arr.pop(30);
+// const arr=["A","G","B","C","H"];
+// arr.sort((a,b)=>a-b);
 // console.log(arr);
 
-// const arr=[10,20,30,40,50];
-// arr.unshift(100);
-// console.log(arr);
+// const arr=[,,,];
+// console.log(arr.length);
+// const arr=[
+//     {name:"Amit",rollno: 3},
+//     {name:"Rohit negi",rollno: 13},
+//     {name:"Gokul",rollno: 18},
+//     {name:"kapil Rathore",rollno: 30},
+//     {name:"Kunal",rollno: 34}
+// ]
+// const  filter=arr.filter(student=>student.rollno===30);
+// console.log(filter);
 
-// const arr=[10,20,30,40,50];
-// console.log(arr.slice(1,3)); //from to from
-// // console.log(arr.splice(1,3)); //from to next add numbers
-// console.log(arr);
+// console.log([]===[]);
+// console.log([]==[]);
 
-// let arr1=[10,20,30,40];
-// let arr2=[50,60,70,80];
-// let arr3=arr1.concat(arr2);
-// console.log(arr3);
-
-// const fun= function()
-// {
-//     console.log("Hello coder Army");
-// }
-// fun();
-
-// const fun=()=>"Helloc";
-// console.log(fun());
+let a=2;
+let b=! --a;
+let c=! --a;
+console.log(b,c);
